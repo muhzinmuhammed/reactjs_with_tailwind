97ffe82d47e9aa3119a6dcd3263c1236dfe4ce38
MUHAMMED MUHZIN T S <muhzinsidhiq333@gmail.com>
2024-10-08 20:03:32 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import Analytics from "./components/Navbar/Analytics"
+import Cards from "./components/Navbar/Cards"
+import Footer from "./components/Navbar/Footer"
+import Hero from "./components/Navbar/Hero"
+import NavBar from "./components/Navbar/NavBar"
+import Newsletter from "./components/Navbar/NewsLetter"
+
+
+function App() {
+  
+
+  return (
+    <>
+ <NavBar/>
+ <Hero/>
+ <Analytics/>
+ <Newsletter/>
+ <Cards/>
+ <Footer/>
+    </>
+  )
+}
+
+export default App
